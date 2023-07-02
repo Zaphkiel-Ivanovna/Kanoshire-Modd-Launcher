a69448182b3b28eed5283ed6b58ec7311dd80099
Zaphkiel <duambi123@gmail.com>
2023-07-03 01:04:24 +0200

    Rework interface

--- src/assets/js/launcher.js
+++ src/assets/js/launcher.js
@@ -23,7 +23,6 @@ class Launcher {
         console.log("Initializing Launcher...");
         if (process.platform == "win32") this.initFrame();
         this.config = await config.GetConfig().then(res => res);
-        this.news = await config.GetNews().then(res => res);
         this.database = await new database().init();
         this.createPanels(Login, Home, Settings);
         this.getaccounts();
@@ -86,7 +85,7 @@ class Launcher {
             div.classList.add("panel", panel.id)
             div.innerHTML = fs.readFileSync(`${__dirname}/panels/${panel.id}.html`, "utf8");
             panelsElem.appendChild(div);
-            new panel().init(this.config, this.news);
+            new panel().init(this.config);
         }
     }
 
